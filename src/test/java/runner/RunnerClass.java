b073844974755ab38b2c4f36e496fc19dd46918e
elkazajo <mzhumakov@gmail.com>
2021-06-23 18:16:04 +0600

    added cucumber runner

--- src/test/java/runner/RunnerClass.java
+++ src/test/java/runner/RunnerClass.java
@@ -8,26 +8,25 @@ import org.testng.xml.XmlSuite;
 import java.util.ArrayList;
 import java.util.List;
 
-@CucumberOptions(strict = true, plugin = { "json:target/cucumber-report.json",
-        "html:target/cucumber-report" }, tags = "@smokeTest", features = "src/test/resources/cucumber/mail_base.feature", glue = {
-        "com.epam.byta.frameworks.demo.steps" })
-
-public class RunnerClass extends AbstractTestNGCucumberTests {
-    public static void main(String[] args) {
-        TestNG testNG = new TestNG();
-
-        XmlSuite xmlSuite = new XmlSuite();
-        xmlSuite.setName("SeleniumHW");
-        List<String> files = new ArrayList<>(new ArrayList<>() {
-            {
-                add("testng.xml");
-            }
-        });
-        xmlSuite.setSuiteFiles(files);
-
-        List<XmlSuite> suites = new ArrayList<>();
-        suites.add(xmlSuite);
-        testNG.setXmlSuites(suites);
-        testNG.run();
-    }
+@CucumberOptions(plugin = { "json:target/cucumber-report.json",
+        "html:target/cucumber-report" }, features = "src/test/resources/cucumber/mail_base.feature", glue = {
+        "src/test/java/steps" })
+public class RunnerClass extends AbstractTestNGCucumberTests{
+//    public static void main(String[] args) {
+//        TestNG testNG = new TestNG();
+//
+//        XmlSuite xmlSuite = new XmlSuite();
+//        xmlSuite.setName("SeleniumHW");
+//        List<String> files = new ArrayList<>(new ArrayList<>() {
+//            {
+//                add("testng.xml");
+//            }
+//        });
+//        xmlSuite.setSuiteFiles(files);
+//
+//        List<XmlSuite> suites = new ArrayList<>();
+//        suites.add(xmlSuite);
+//        testNG.setXmlSuites(suites);
+//        testNG.run();
+//    }
 }
--- src/test/java/steps/MailLoginStep.java
+++ src/test/java/steps/MailLoginStep.java
@@ -1,5 +1,7 @@
 package steps;
 
+import business_object.User;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -9,30 +11,60 @@ import page.LoginPage;
 import utils.WebDriverCreator;
 
 public class MailLoginStep {
-    WebDriver driver = WebDriverCreator.getDriver();
+    WebDriver driver;
+    LoginPage loginPage;
+    User user;
+
+    @Before
+    public void setUp() {
+        driver = WebDriverCreator.getDriver();
+        loginPage = new LoginPage(driver);
+        user = new User();
+    }
 
     @Given("user navigates to the mail page")
-    public void navigateToMailPage() {
-        new LoginPage(driver).openPage();
+    public void user_navigates_to_the_mail_page() {
+        try {
+            loginPage.openPage();
+        } catch (io.cucumber.java.PendingException ignored) {
+
+        }
     }
 
     @When("page opens enters username")
-    public void enterUserName(String userName) {
-        new LoginPage(driver).inputUserName(userName);
+    public void page_opens_enters_username() {
+        try {
+            loginPage.inputUserName(user.getName());
+        } catch (io.cucumber.java.PendingException ignored) {
+
+        }
+
     }
 
     @And("presses enter button")
-    public void pressEnterPasswordButton() {
-        new LoginPage(driver).enterPasswordButton();
+    public void presses_enter_button() {
+        try {
+            loginPage.enterPasswordButton();
+        } catch (io.cucumber.java.PendingException ignored) {
+
+        }
     }
 
     @And("when the password form appears enters password")
-    public void enterPassword(String password) {
-        new LoginPage(driver).inputPassword(password);
+    public void when_the_password_form_appears_enters_password(String password) {
+        try {
+            loginPage.inputPassword(user.getPassword());
+        } catch (io.cucumber.java.PendingException ignored) {
+
+        }
     }
 
     @Then("presses login button to enter mail's main page")
-    public void login() {
-        new LoginPage(driver).clickLoginButton();
+    public void presses_login_button_to_enter_mail_s_main_page() {
+        try {
+            loginPage.clickLoginButton();
+        } catch (io.cucumber.java.PendingException ignored) {
+
+        }
     }
 }
